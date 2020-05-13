e3e8dd8b465b0e4c43485c486200e2fcc1d5575c
Krishnakant Sharma <krishnakant.sharma@semasoft.co.in>
2020-05-13 21:46:05 +0530

    arbind Changes 13 May 9.45

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,111 +1,119 @@
 <template>
   <div class="home">
-    <v-app-bar color="#F05A00" dense dark>
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
 
-      <v-toolbar-title>Bhojmandu</v-toolbar-title>
+    <Header/>
 
-      <v-spacer></v-spacer>
+    <!-- Image with Search Box -->
+    
+      <v-img :aspect-ratio="16/4" src="../assets/bhojmandu.png"> 
 
-      <v-icon>restaurant_menu</v-icon>
-      <v-btn text class="ma-1">Feels Like Eating</v-btn>
-      <v-icon>location_on</v-icon>
-      <v-btn text class="ma-1">Choose A Location</v-btn>
-      <v-btn text class="ma-1">Login</v-btn>
-      <v-menu left bottom></v-menu>
-    </v-app-bar>
-
-    <v-divider class="mx-4" horizontal></v-divider>
-    <v-row align="center" justify="center">
-      <v-img
-        src="../assets/bhojmandu.png"
-        aspect-ratio="1"
-        class="grey lighten-2"
-        max-width="100%"
-        max-height="300"
-      >
-        <template v-slot:placeholder>
-          <v-row class="fill-height ma-0" align="center" justify="center">
-            <v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
-          </v-row>
-        </template>
+      <v-container>       
+        <v-row class="white--text">
+        <p class="display-2 white--text">Order Food Online</p>
+      </v-row>
+      <v-row>
+        <p class="font-weight-regular white--text">"Best restaurants in Kathmandu delivering to your doorstep"</p>
+      </v-row>
+      <v-row>
+        <v-col cols="12" sm="3" md="3">
+        <v-select
+        prepend-inner-icon="location_on"
+          :items="city_dropdown"
+         label="Kathamndu"
+         solo
+         dense
+        ></v-select>
+      </v-col>
+        <v-col cols="12" sm="6" md="6">
+          <v-text-field background-color="#fff" dense label="Type delivery location here..." single-line outlined prepend-inner-icon="near_me" suffix="Hello thet n"></v-text-field>
+                  
+        </v-col>
+        <v-col cols="12" sm="3" md="3" lg="3">
+          <v-btn color="green">Find Resturent </v-btn>                  
+        </v-col>
+      </v-row>
+      </v-container>
       </v-img>
-    </v-row>
-    <v-container class="mt-10">
+      
+    <!-- End of Image with search Box -->
+
+     <v-container class="mt-10">
       <v-row class="center" justify="center">
-        <v-text class="display-2">Now, We Are Accepting Order</v-text>
+        <p class="display-2">Top Categories</p>
       </v-row>
-      <v-row class="center mt-5" justify="center">
-        <v-text class="title">"Taste Something Different"</v-text>
-      </v-row>
-      <v-row>
-        <v-col>
-          <v-text-field outlined label="Resturant Name" prepend-inner-icon="restaurant_menu"></v-text-field>
-        </v-col>
-        <v-col>
-          <v-text-field outlined label="Restaurant Location" prepend-inner-icon="location_on"></v-text-field>
-        </v-col>
+     <v-row class="center" justify="center">
+          <p class="title">Browse by Categories</p>
       </v-row>
     </v-container>
 
-    <v-container class="mt-10">
-      <v-row class="center" justify="center">
-        <v-text class="display-2">Top Restaurants we are Parter with !!!</v-text>
-      </v-row>
-    </v-container>
-    <v-container>
-      <v-row>
-        <v-col cols="4" v-for="n in 6" :key="n">
-          <v-card class="mx-auto" max-width="344" outlined>
-            <v-list-item three-line>
-              <v-list-item-content>
-                <div class="overline mb-4">Name of Restaurant</div>
-                <v-list-item-title class="headline mb-1">Restaurant</v-list-item-title>
-                <v-list-item-subtitle>Little description of Restaurant goes here.</v-list-item-subtitle>
-              </v-list-item-content>
+  <v-sheet
+    class="mx-auto"
+    elevation="8"
+    max-width="100%"
+  >
+    <v-slide-group
+      v-model="model"
+      class="pa-4"
+      multiple
+      show-arrows
+    >
+      <v-slide-item
+        v-for="n in 15"
+        :key="n"
+        v-slot:default="{ active, toggle }"
+      >
+        <v-card
+          :color="active ? 'primary' : 'grey lighten-1'"
+          class="ma-4"
+          height="200"
+          width="100"
+          @click="toggle"
+        >
+          <v-row
+            class="fill-height"
+            align="center"
+            justify="center"
+          >
+            <v-scale-transition>
+              <v-icon
+                v-if="active"
+                color="white"
+                size="80"
+                v-text="'mdi-close-circle-outline'"
+              ></v-icon>
+            </v-scale-transition>
+          </v-row>
+        </v-card>
+      </v-slide-item>
+    </v-slide-group>
+  </v-sheet>
 
-              <v-img
-                height="100%"
-                width="10"
-                src="https://cdn.vuetifyjs.com/images/cards/store.jpg"
-              ></v-img>
-            </v-list-item>
+    <Restaurant/> 
 
-            <v-card-actions>
-              <v-btn text>Link1</v-btn>
-              <v-btn text>Link2</v-btn>
-            </v-card-actions>
-          </v-card>
-        </v-col>
-      </v-row>
-    </v-container>
+    <Footer/>
 
-    <v-footer dark padless>
-      <v-card class="flex" flat tile>
-        <v-card-title class="teal">
-          <strong class="subheading">Get connected with us on social networks!</strong>
-
-          <v-spacer></v-spacer>
-
-          <v-btn v-for="icon in icons" :key="icon" class="mx-4" dark icon>
-            <v-icon size="24px">{{ icon }}</v-icon>
-          </v-btn>
-        </v-card-title>
-
-        <v-card-text class="py-2 white--text text-center">
-          Copyright &copy; {{ new Date().getFullYear() }} —
-          <strong>Bhojmandu. All Right Reserved.</strong>
-        </v-card-text>
-      </v-card>
-    </v-footer>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
+
+import Header from  "../components/header/header";
+import Restaurant from "../components/restaurant/restaurant"
+import Footer from "../components/footer/footer";
 
 export default {
-  name: "Home",
-  components: {}
+
+  components: { Header, Restaurant, Footer  },
+  data: () => ({
+      icons: [
+        'mdi-facebook',
+        'mdi-instagram',
+        'mdi-youtube',
+      ],
+      city_dropdown: ['Kathmandu', 'Bhaktapur', 'Lalitpur', 'Chitwan'],
+      model: [],
+    }),
+  name: "Home",  
 };
+ 
 </script>
